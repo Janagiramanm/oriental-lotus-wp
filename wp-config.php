3e052e8287e9039fb20e2f7ad2304f73e50c9efd
Janagiramanm <janagiraman@netiapps.com>
2023-02-08 15:54:00 +0530

    debug

--- wp-config.php
+++ wp-config.php
@@ -25,10 +25,10 @@
 define( 'DB_NAME', 'oriental_lotus_wp' );
 
 /** Database username */
-define( 'DB_USER', '$QgIcsnU8QNnJ7&AA1' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '$QgIcsnU8QNnJ7&AA1' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
