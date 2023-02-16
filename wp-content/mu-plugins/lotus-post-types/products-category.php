e5b5844cd9e99c7e4bcd4852d5b42317dce9bf6a
Janagiramanm <janagiraman@netiapps.com>
2023-02-16 10:22:40 +0530

    debug

--- wp-content/mu-plugins/lotus-post-types/products-category.php
+++ wp-content/mu-plugins/lotus-post-types/products-category.php
@@ -2,19 +2,19 @@
 
 function setup_product_category_page() {
 	register_post_type("product-category", [
-		"label" => "Product Category",
+		"label" => "Product Overview",
 		"labels" => [
 			"name" => "Product Overview",
 			"singular_name" => "product-category",
 			"add_new_item" => "Add New Product Category",
-			"edit_item" => "Edit Product Category",
-			"view_item" => "View Product Category",
-			"view_items" => "View Product Categories",
-			"not_found" => "No Product Category found",
-			"all_items" => "All Product Categories",
-			"archives" => "Product Category Page archives"
+			"edit_item" => "Edit Product Overview",
+			"view_item" => "View Product Overview",
+			"view_items" => "View Product Overview",
+			"not_found" => "No Product Overview found",
+			"all_items" => "All Overviews",
+			"archives" => "Product Overview Page archives"
 		],
-		"description" => "Product Category Page provided by Oriental Lotus",
+		"description" => "Product Overview Page provided by Oriental Lotus",
 		"public" => true,
 		'has_archive' => true,
 		"show_in_rest" => true,
